3f89166f88b2a16482f4cfd153d8c921e91ae0b3
jkClyde <aquin.randall@gmail.com>
2025-01-28 02:41:34 +0800

    icons-fix

--- public/icons/index.js
+++ public/icons/index.js
@@ -1,7 +1,7 @@
 // Instead of imports, use public URLs:
-const icon1 = '/icons/icon-1.png';
-const icon2 = '/icons/icon-2.png';
-const icon3 = '/icons/icon-3.png';
-const icon4 = '/icons/icon.png';
+const icon1 = "/icons/icon-1.png";
+const icon2 = "/icons/icon-2.png";
+const icon3 = "/icons/icon-3.png";
+const icon4 = "/icons/icon.png";
 
 export { icon1, icon2, icon3, icon4 };
